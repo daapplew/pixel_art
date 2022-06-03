6cc0503dbda3f35f7b33ed68ef050d109ddafae0
Dwayne Applewhite <dwayneaapplewhite@gmail.com>
2022-06-03 01:48:26 -0400

    updated the designs.js file

--- designs.js
+++ designs.js
@@ -1,11 +1,38 @@
 // Select color input
 // Select size input
 
+const color = document.getElementById("colorPicker");
+const size = document.getElementById("sizePicker"); 
+const table = document.getElementById("pixelCanvas"); 
+
 // When size is submitted by the user, call makeGrid()
+size.addEventListener("submit", function (event){
+    // variables for both height and width input values
+    const height = document.getElementById("inputHeight").value; 
+    const width = document.getElementById("inputWidth").value;
 
-function makeGrid() {
-let height = document.getElementById("inputHeight")
-let width = document.getElementById("inputWidth")
-// Your code goes here!
+    table.innerHTML = " "; // empty the canvas
+    event.preventDefault(); 
+    makeGrid(height,width);
+});
 
-}
+// add color to table data based on the input set by user
+table.addEventListener("click", function(event){
+    if (event.target.nodeName === "TD"){
+        event.target.style = `background-color: ${color.value};`;
+    };
+});
+
+// make a grid function that takes in 2 arguments
+function makeGrid(height,width){
+    //for loop for the tr aka height that creates rows
+    for (let i = 0; i < height; i++ ){
+        let row = table.appendChild(document.createElement('tr'));
+        //for loop for the td aka width that creates cells
+        for (let j = 0; j < width; j++){
+            row.appendChild(document.createElement('td'));
+        };
+    };
+};
+
+
